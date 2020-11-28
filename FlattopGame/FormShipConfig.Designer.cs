414e9f673c405404d42f1f61b7d1a87210484e19
vova.emiryan <vova.emiryan@yandex.ru>
2020-11-28 21:00:40 +0400

    new form

---
+++ FlattopGame/FormShipConfig.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace FlattopGame
+{
+	partial class FormShipConfig
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.typeGroupBox = new System.Windows.Forms.GroupBox();
+			this.flattopLabel = new System.Windows.Forms.Label();
+			this.armyShipLabel = new System.Windows.Forms.Label();
+			this.paramGroupBox = new System.Windows.Forms.GroupBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.shipWeightLabel = new System.Windows.Forms.Label();
+			this.maxSpeedLabel = new System.Windows.Forms.Label();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.pictureBoxShip = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.panel5 = new System.Windows.Forms.Panel();
+			this.panel6 = new System.Windows.Forms.Panel();
+			this.panel7 = new System.Windows.Forms.Panel();
+			this.panel8 = new System.Windows.Forms.Panel();
+			this.panel9 = new System.Windows.Forms.Panel();
+			this.mainColorLabel = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.typeGroupBox.SuspendLayout();
+			this.paramGroupBox.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxShip)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// typeGroupBox
+			// 
+			this.typeGroupBox.Controls.Add(this.flattopLabel);
+			this.typeGroupBox.Controls.Add(this.armyShipLabel);
+			this.typeGroupBox.Location = new System.Drawing.Point(12, 12);
+			this.typeGroupBox.Name = "typeGroupBox";
+			this.typeGroupBox.Size = new System.Drawing.Size(143, 106);
+			this.typeGroupBox.TabIndex = 0;
+			this.typeGroupBox.TabStop = false;
+			this.typeGroupBox.Text = "Тип корабля";
+			// 
+			// flattopLabel
+			// 
+			this.flattopLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.flattopLabel.Location = new System.Drawing.Point(9, 58);
+			this.flattopLabel.Name = "flattopLabel";
+			this.flattopLabel.Size = new System.Drawing.Size(125, 35);
+			this.flattopLabel.TabIndex = 1;
+			this.flattopLabel.Text = "Авианосец";
+			this.flattopLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// armyShipLabel
+			// 
+			this.armyShipLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.armyShipLabel.Location = new System.Drawing.Point(9, 19);
+			this.armyShipLabel.Name = "armyShipLabel";
+			this.armyShipLabel.Size = new System.Drawing.Size(125, 35);
+			this.armyShipLabel.TabIndex = 0;
+			this.armyShipLabel.Text = "Военный корабль";
+			this.armyShipLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// paramGroupBox
+			// 
+			this.paramGroupBox.Controls.Add(this.checkBox4);
+			this.paramGroupBox.Controls.Add(this.checkBox3);
+			this.paramGroupBox.Controls.Add(this.checkBox2);
+			this.paramGroupBox.Controls.Add(this.checkBox1);
+			this.paramGroupBox.Controls.Add(this.shipWeightLabel);
+			this.paramGroupBox.Controls.Add(this.maxSpeedLabel);
+			this.paramGroupBox.Controls.Add(this.numericUpDown2);
+			this.paramGroupBox.Controls.Add(this.numericUpDown1);
+			this.paramGroupBox.Location = new System.Drawing.Point(12, 124);
+			this.paramGroupBox.Name = "paramGroupBox";
+			this.paramGroupBox.Size = new System.Drawing.Size(374, 119);
+			this.paramGroupBox.TabIndex = 1;
+			this.paramGroupBox.TabStop = false;
+			this.paramGroupBox.Text = "Параметры";
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.AutoSize = true;
+			this.checkBox4.Location = new System.Drawing.Point(237, 81);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(80, 17);
+			this.checkBox4.TabIndex = 7;
+			this.checkBox4.Text = "checkBox4";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(237, 62);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(80, 17);
+			this.checkBox3.TabIndex = 6;
+			this.checkBox3.Text = "checkBox3";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(237, 42);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(80, 17);
+			this.checkBox2.TabIndex = 5;
+			this.checkBox2.Text = "checkBox2";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(237, 23);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(80, 17);
+			this.checkBox1.TabIndex = 4;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// shipWeightLabel
+			// 
+			this.shipWeightLabel.AutoSize = true;
+			this.shipWeightLabel.Location = new System.Drawing.Point(33, 71);
+			this.shipWeightLabel.Name = "shipWeightLabel";
+			this.shipWeightLabel.Size = new System.Drawing.Size(74, 13);
+			this.shipWeightLabel.TabIndex = 3;
+			this.shipWeightLabel.Text = "Вес корабля:";
+			// 
+			// maxSpeedLabel
+			// 
+			this.maxSpeedLabel.AutoSize = true;
+			this.maxSpeedLabel.Location = new System.Drawing.Point(33, 32);
+			this.maxSpeedLabel.Name = "maxSpeedLabel";
+			this.maxSpeedLabel.Size = new System.Drawing.Size(90, 13);
+			this.maxSpeedLabel.TabIndex = 2;
+			this.maxSpeedLabel.Text = "Макс. скорость:";
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Location = new System.Drawing.Point(129, 29);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(50, 20);
+			this.numericUpDown2.TabIndex = 1;
+			this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(129, 68);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
+			this.numericUpDown1.TabIndex = 0;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+			// 
+			// pictureBoxShip
+			// 
+			this.pictureBoxShip.Location = new System.Drawing.Point(172, 25);
+			this.pictureBoxShip.Name = "pictureBoxShip";
+			this.pictureBoxShip.Size = new System.Drawing.Size(200, 80);
+			this.pictureBoxShip.TabIndex = 2;
+			this.pictureBoxShip.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(161, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(225, 106);
+			this.panel1.TabIndex = 8;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.mainColorLabel);
+			this.groupBox1.Controls.Add(this.panel9);
+			this.groupBox1.Controls.Add(this.panel8);
+			this.groupBox1.Controls.Add(this.panel7);
+			this.groupBox1.Controls.Add(this.panel6);
+			this.groupBox1.Controls.Add(this.panel4);
+			this.groupBox1.Controls.Add(this.panel5);
+			this.groupBox1.Controls.Add(this.panel3);
+			this.groupBox1.Controls.Add(this.panel2);
+			this.groupBox1.Location = new System.Drawing.Point(410, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(224, 146);
+			this.groupBox1.TabIndex = 9;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.Red;
+			this.panel2.Location = new System.Drawing.Point(23, 65);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(30, 30);
+			this.panel2.TabIndex = 0;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.White;
+			this.panel3.Location = new System.Drawing.Point(167, 65);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(30, 30);
+			this.panel3.TabIndex = 1;
+			// 
+			// panel4
+			// 
+			this.panel4.BackColor = System.Drawing.Color.Black;
+			this.panel4.Location = new System.Drawing.Point(121, 65);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(30, 30);
+			this.panel4.TabIndex = 1;
+			// 
+			// panel5
+			// 
+			this.panel5.BackColor = System.Drawing.Color.Yellow;
+			this.panel5.Location = new System.Drawing.Point(73, 65);
+			this.panel5.Name = "panel5";
+			this.panel5.Size = new System.Drawing.Size(30, 30);
+			this.panel5.TabIndex = 1;
+			// 
+			// panel6
+			// 
+			this.panel6.BackColor = System.Drawing.Color.Gray;
+			this.panel6.Location = new System.Drawing.Point(23, 101);
+			this.panel6.Name = "panel6";
+			this.panel6.Size = new System.Drawing.Size(30, 30);
+			this.panel6.TabIndex = 1;
+			// 
+			// panel7
+			// 
+			this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+			this.panel7.Location = new System.Drawing.Point(73, 101);
+			this.panel7.Name = "panel7";
+			this.panel7.Size = new System.Drawing.Size(30, 30);
+			this.panel7.TabIndex = 1;
+			// 
+			// panel8
+			// 
+			this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.panel8.Location = new System.Drawing.Point(121, 101);
+			this.panel8.Name = "panel8";
+			this.panel8.Size = new System.Drawing.Size(30, 30);
+			this.panel8.TabIndex = 1;
+			// 
+			// panel9
+			// 
+			this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+			this.panel9.Location = new System.Drawing.Point(167, 101);
+			this.panel9.Name = "panel9";
+			this.panel9.Size = new System.Drawing.Size(30, 30);
+			this.panel9.TabIndex = 1;
+			// 
+			// mainColorLabel
+			// 
+			this.mainColorLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.mainColorLabel.Location = new System.Drawing.Point(23, 19);
+			this.mainColorLabel.Name = "mainColorLabel";
+			this.mainColorLabel.Size = new System.Drawing.Size(85, 35);
+			this.mainColorLabel.TabIndex = 2;
+			this.mainColorLabel.Text = "Основной цвет";
+			this.mainColorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label2.Location = new System.Drawing.Point(112, 19);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(85, 35);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Доп. цвет";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// FormShipConfig
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(647, 255);
+			this.Controls.Add(this.pictureBoxShip);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.paramGroupBox);
+			this.Controls.Add(this.typeGroupBox);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "FormShipConfig";
+			this.Text = "FormShipConfig";
+			this.typeGroupBox.ResumeLayout(false);
+			this.paramGroupBox.ResumeLayout(false);
+			this.paramGroupBox.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxShip)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox typeGroupBox;
+		private System.Windows.Forms.GroupBox paramGroupBox;
+		private System.Windows.Forms.PictureBox pictureBoxShip;
+		private System.Windows.Forms.Label flattopLabel;
+		private System.Windows.Forms.Label armyShipLabel;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Label shipWeightLabel;
+		private System.Windows.Forms.Label maxSpeedLabel;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label mainColorLabel;
+		private System.Windows.Forms.Panel panel9;
+		private System.Windows.Forms.Panel panel8;
+		private System.Windows.Forms.Panel panel7;
+		private System.Windows.Forms.Panel panel6;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Panel panel5;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel2;
+	}
+}
---
+++ FlattopGame/FormShipConfig.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlattopGame
+{
+	public partial class FormShipConfig : Form
+	{
+		/// <summary>
+		/// Переменная-выбранная машина
+		/// </summary>
+		ITransport armyShip = null;
+		public FormShipConfig()
+		{
+			InitializeComponent();
+		}
+		/// <summary>
+		/// Отрисовать машину
+		/// </summary>
+		private void DrawShip()
+		{
+			if (armyShip != null)
+			{
+				Bitmap bmp = new Bitmap(pictureBoxShip.Width, pictureBoxShip.Height);
+				Graphics gr = Graphics.FromImage(bmp);
+				armyShip.SetPosition(5, 5, pictureBoxShip.Width, pictureBoxShip.Height);
+				armyShip.DrawTransport(gr);
+				pictureBoxShip.Image = bmp;
+			}
+		}
+		/// <summary>
+		/// Передаем информацию при нажатии на Label
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void armyShipLabel_MouseDown(object sender, MouseEventArgs e)
+		{
+			armyShipLabel.DoDragDrop(armyShipLabel.Text, DragDropEffects.Move | DragDropEffects.Copy);
+	    }
+		/// <summary>
+		/// Передаем информацию при нажатии на Label
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void flattopLabel_MouseDown(object sender, MouseEventArgs e)
+		{
+			flattopLabel.DoDragDrop(flattopLabel.Text, DragDropEffects.Move | DragDropEffects.Copy);
+		}
+		/// <summary>
+		/// Проверка получаемой информации (ее типа на соответствие требуемому)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void shipPanel_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.Text))
+			{
+				e.Effect = DragDropEffects.Copy;
+			}
+			else
+			{
+				e.Effect = DragDropEffects.None;
+			}
+		}
+		/// <summary>
+		/// Действия при приеме перетаскиваемой информации
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void shipPanel_DragDrop(object sender, DragEventArgs e)
+		{
+			switch (e.Data.GetData(DataFormats.Text).ToString())
+			{
+				case "Военный корабль":
+					armyShip = new ArmyShip(100, 500, Color.White);
+					break;
+				case "Авианосец":
+					armyShip = new Flattop(100, 500, Color.White, Color.Black, true, true,true,true,true);
+					break;
+			}
+			DrawShip();
+		}
+	}
+}
